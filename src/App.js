923cd183b1dc9a34edbba3064d1dafdcb8f51b84
praveen <pprav2508@gmail.com>
2020-04-13 19:51:32 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Covid Central</h1>
+        <h3>coming soon..</h3>
+
+    </div>
+  );
+}
+
+export default App;
