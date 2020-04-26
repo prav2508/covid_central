ef7792475022e3f00c97969784caa1beb313251d
Praveen <pprav2508@gmail.com>
2020-04-26 07:20:22 +0530

    test

--- src/components/insights.js
+++ src/components/insights.js
@@ -21,25 +21,25 @@ function Insights() {
     const [timeline, setTimeline] = useState([0])
 
 
-    useEffect(async () => {
-        await axios.get('/timeline')
-            .then(res => {
-                //setGlobal_country(res.data)
-                setC_conf(res.data.data[0].confirmed)
-                setC_act(res.data.data[0].active)
-                setC_recv(res.data.data[0].recovered)
-                setC_death(res.data.data[0].deaths)
-                setTimeline(res.data.data.sort((a,b)=>a.date>b.date?1:-1))
-            }).catch(err => {
-                console.log(err)
-            })
-       await axios.get('/countries')
-            .then(res => {
-               setGlobal_country(res.data.data)
-               console.log(res.data)
-            }).catch(err => {
-                console.log(err)
-            })
+    useEffect(() => {
+        //  axios.get('/timeline')
+        //     .then(res => {
+        //         //setGlobal_country(res.data)
+        //         setC_conf(res.data.data[0].confirmed)
+        //         setC_act(res.data.data[0].active)
+        //         setC_recv(res.data.data[0].recovered)
+        //         setC_death(res.data.data[0].deaths)
+        //         setTimeline(res.data.data.sort((a,b)=>a.date>b.date?1:-1))
+        //     }).catch(err => {
+        //         console.log(err)
+        //     })
+        // axios.get('/countries')
+        //     .then(res => {
+        //        setGlobal_country(res.data.data)
+        //        console.log(res.data)
+        //     }).catch(err => {
+        //         console.log(err)
+        //     })
     },[]);
 
     const change = (e) => {
